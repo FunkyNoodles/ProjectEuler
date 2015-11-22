3ccc7327218444b8a2e0836598bdf29330e62fcf
Funky Noodles <nickasds@hotmail.com>
2015-11-21 21:00:49 -0600

    Started Problem 14

---
+++ ProjectEuler/ProjectEuler/Problem14.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "Problem14.h"
+#include <iostream>
+
+using namespace std;
+
+Problem14::Problem14()
+{
+}
+
+Problem14::~Problem14()
+{
+}
+
+int Problem14::findCollatzNumber(unsigned int n)
+{
+	return 0;
+}
---
+++ ProjectEuler/ProjectEuler/Problem14.h
@@ -0,0 +1,9 @@
+#pragma once
+class Problem14
+{
+public:
+	Problem14();
+	~Problem14();
+	int findCollatzNumber(unsigned int n);
+};
+
