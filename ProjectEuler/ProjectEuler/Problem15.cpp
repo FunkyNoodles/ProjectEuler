33c77daa0ae7cdbf304bde251f4a0ebd718b3b82
Funky Noodles <nickasds@hotmail.com>
2015-11-24 12:16:36 -0600

    Cleaned up problem 15 code

--- ProjectEuler/ProjectEuler/Problem15.cpp
+++ ProjectEuler/ProjectEuler/Problem15.cpp
@@ -32,21 +32,3 @@ Problem15::Problem15()
 Problem15::~Problem15()
 {
 }
-
-void Problem15::findPathNumber(int curX, int curY, int tarX, int tarY, int gridWidth, int gridHeight, unsigned __int64 * count)
-{
-	if (grid[curX][curY] == 2)
-	{
-		cout << *count << endl;
-		*count = *count + 1;
-		return;
-	}
-	if (curX < gridWidth)
-	{
-		Problem15::findPathNumber(curX + 1, curY, gridWidth - 1, gridHeight - 1, gridWidth, gridHeight, count);
-	}
-	if (curY < gridHeight)
-	{
-		Problem15::findPathNumber(curX, curY + 1, gridWidth - 1, gridHeight - 1, gridWidth, gridHeight, count);
-	}
-}
--- ProjectEuler/ProjectEuler/Problem15.h
+++ ProjectEuler/ProjectEuler/Problem15.h
@@ -4,7 +4,5 @@ class Problem15
 public:
 	Problem15();
 	~Problem15();
-	void findPathNumber(int curX, int curY, int tarX, int tarY, int gridWidth, int gridHeight, unsigned __int64 *count);
-	
 };
 
